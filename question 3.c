cc97d253956b26304080a252b96e29999d13ad42
Abdur Rahman Ahmed Bakhshi <k253096@nu.edu.pk>
2025-11-14 22:14:54 +0500

    Update question 3.c

--- question 3.c
+++ question 3.c
@@ -2,16 +2,19 @@
 
 int main() {
     int n;
+    printf("Enter n: ");
     scanf("%d", &n);
-
-    int nums[100]; 
-
-    for(int i = 0; i < n; i++) scanf("%d", &nums[i]);
-
-    for(int i = 0; i < n; i++) nums[i] = nums[i] % 2 ? 1 : 0;
-
-    for(int i = 0; i < n - 1; i++) {
-        for(int j = i + 1; j < n; j++) {
+    int i, j;
+    int nums[100];
+    printf("Enter %d numbers: ", n);
+    for(i = 0; i < n; i++){
+        scanf("%d", &nums[i]);
+    }
+    for(i = 0; i < n; i++) {
+        nums[i] = nums[i] % 2 ? 1 : 0;
+    }
+    for(i = 0; i < n - 1; i++) {
+        for(j = i + 1; j < n; j++) {
             if(nums[i] > nums[j]) {
                 int t = nums[i];
                 nums[i] = nums[j];
@@ -19,9 +22,9 @@ int main() {
             }
         }
     }
-
-    for(int i = 0; i < n; i++) 
-    {printf("%d ", nums[i]);}
-
+    printf("Output: ");
+    for(i = 0; i < n; i++){
+        printf("%d ", nums[i]);
+    }
     return 0;
 }
